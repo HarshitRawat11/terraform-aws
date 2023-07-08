f0cf101749c1f3d754d72244f83334d80deca5c3
Harshit Rawat <harshitrawat@outlook.com>
2023-07-08 23:14:03 +0530

    Created rds setup and vpc outputs for rds

--- main.tf
+++ main.tf
@@ -10,3 +10,17 @@ module "networking" {
   max_subnet       = 10
   db_subnet_group  = true
 }
+
+module "database" {
+  source                 = "./database"
+  db_storage             = 10
+  db_engine_version      = "5.7.22"
+  db_instance_class      = "db.t2.micro"
+  dbname                 = "terraform-rds"
+  dbuser                 = "rawat"
+  dbpassword             = "rawat@1234"
+  db_identifier          = "rds-db"
+  skip_db_snapshot       = true
+  db_subnet_group_name   = module.networking.db_subnet_group_name[0]
+  vpc_security_group_ids = module.networking.db_security_group_ids
+}
--- networking/outputs.tf
+++ networking/outputs.tf
@@ -1,3 +1,11 @@
 output "vpc_id" {
   value = aws_vpc.custom_vpc.id
 }
+
+output "db_subnet_group_name" {
+  value = aws_db_subnet_group.rds_subnet_group.*.name
+}
+
+output "db_security_group_ids" {
+  value = [aws_security_group.custom_sg["rds"].id]
+}
