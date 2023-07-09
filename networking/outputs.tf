58f7a6c9bfdebf24f1c46b99a381791a2918e3fb
Harshit Rawat <harshitrawat@outlook.com>
2023-07-09 12:50:58 +0530

    Removed vpc security group ids from rds resource till the error fixes

--- networking/outputs.tf
+++ networking/outputs.tf
@@ -6,6 +6,6 @@ output "db_subnet_group_name" {
   value = aws_db_subnet_group.rds_subnet_group.*.name
 }
 
-output "db_security_group_ids" {
-  value = [aws_security_group.custom_sg["rds"].id]
-}
+# output "db_security_group_ids" {
+#   value = [aws_security_group.custom_sg.ingress.rds.id]
+# }
--- variables.tf
+++ variables.tf
@@ -18,3 +18,21 @@ variable "access_ip" {
   type        = string
   description = "list of ip address allowed for ingress rule in public security group"
 }
+
+variable "dbname" {
+  type        = string
+  description = "name of the db"
+}
+
+variable "dbuser" {
+  type        = string
+  description = "name of the user of the db"
+  sensitive   = true
+}
+
+variable "dbpassword" {
+  type        = string
+  description = "password of the db"
+  sensitive   = true
+
+}
